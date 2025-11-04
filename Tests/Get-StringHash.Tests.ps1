91dc669e9300c1d8f41915ed3f1ee4387338b750
dgswilkins <dgswilkins@users.noreply.github.com>
2025-11-04 14:50:42 -0500

    Supress spurious devskim warnings

--- Tests/Get-StringHash.Tests.ps1
+++ Tests/Get-StringHash.Tests.ps1
@@ -6,16 +6,16 @@
 Describe "Get-StringHash" {
     It "Returns correct SHA256 hash for 'hello'" {
         $result = Get-StringHash -String "hello" -HashName "SHA256"
-        $expected = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+        $expected = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824" # DevSkim: ignore DS173237 because it is a known hash value
         $result | Should -Be $expected
     }
     It "Returns correct SHA512 hash for 'hello'" {
         $result = Get-StringHash -String "hello" -HashName "SHA512"
-        $expected = "9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043"
+        $expected = "9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043" # DevSkim: ignore DS173237 because it is a known hash value 
         $result | Should -Be $expected
     }
     It "Throws for unsupported algorithm" {
-        { Get-StringHash -String "hello" -HashName "MD5" } | Should -Throw
+        { Get-StringHash -String "hello" -HashName "MD5" } | Should -Throw # DevSkim: ignore DS126858 because we are testing unsupported algorithm handling
     }
     It "Throws for missing string parameter" {
         { Get-StringHash } | Should -Throw
@@ -23,8 +23,8 @@ Describe "Get-StringHash" {
     It "Supports pipeline input" {
         $results = @("hello", "world") | Get-StringHash -HashName "SHA256"
         $expected = @(
-            "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
-            "486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7"
+            "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", # DevSkim: ignore DS173237 because it is a known hash value
+            "486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7" # DevSkim: ignore DS173237 because it is a known hash value
         )
         $results | Should -Be $expected
     }
